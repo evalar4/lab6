5a6fcfef28c47b0f5e236fcd7fe695e1e8162f1b
evalar4 <evelinka-07@mail.ru>
2025-06-11 11:05:13 +0300

    lan6 done

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from lab06!" << std::endl;
+    std::cout << "This is a C++ project built with CMake" << std::endl;
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <cassert>
+
+void test_calculation() {
+    assert(2 + 2 == 4);
+}
+
+int main() {
+    test_calculation();
+    return 0;
+}
